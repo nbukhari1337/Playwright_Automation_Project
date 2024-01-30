b7cabdfbecc72908360eb9396e9ff0e933411c74
nbukhari1337 <nbukhari1337@gmail.com>
2024-01-29 22:02:03 -0500

    first commit

---
+++ src/test/GoogleSearch.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect, Page } from '@playwright/test'
+
+//any variable can be defined with let or const
+let page:Page
+
+//create beforeAll function to define only one instance of the page
+test.beforeAll(async({browser}) => {
+    page = await browser.newPage()
+})
+
+test("search for bmw on google search field @smoke",async() => {
+//print out steps to console (sys.out in java)
+console.log("Navigating to google home page")
+//navigate to google
+await page.goto("https://www.google.com")
+//wait few seconds (thread.sleep in java)
+await page.waitForTimeout(2000)
+//type bmw in the search field
+await page.locator("xpath = //*[@name='q']").fill("BMW")
+console.log("Typing bmw in the search field")
+//click on the search button
+await page.locator("xpath = //*[@name='btnK']").nth(0).click()
+console.log("Clicked on google search button")
+//capture the search result
+let result = await page.locator("xpath = //*[@id='result-stats']").textContent()
+console.log("Captured the search result")
+//split the search result
+let resultArray = result?.split(" ")
+//print out only the search result number
+console.log("Search result number is " + resultArray[1])
+})
